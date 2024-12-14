2357b1fc69f1ebeeaae291d4495b72c46aea8cbc
nanasnumber <nanasnumber@gmail.com>
2024-12-14 11:59:41 -0500

    update

--- pack.sh
+++ pack.sh
@@ -22,4 +22,4 @@ convert $cover -resize 1920x -gravity center -crop 1920x1080+0+0 +repage yt-thum
 # generate the full length video
 ffmpeg -loop 1 -i ./yt-thumb.jpg -i ./audio.wav -c:v libx264 -c:a aac -b:a 320k -shortest -pix_fmt yuv420p ./dist/16-9.mp4
 ffmpeg -loop 1 -i ./cover.png -i ./audio.wav -c:v libx264 -c:a aac -b:a 320k -shortest -pix_fmt yuv420p ./dist/1-1.mp4
-
+ffmpeg -i ./dist/1-1.mp4 -ss 00:00:00 -to 00:00:55 ./dist/1-1--55s.mp4
